61f5151050c585a7a477689e435227fb580c81a4
TomySNM <tsanmartin@usm.cl>
2024-10-19 23:43:32 -0300

    sistema cambios y actualizaciones (2)

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from.models import Agregar
+from.models import Agregar, Elemento, Notificacion
 
 #Para que tambien se registre en Django Admin
 admin.site.register(Agregar)
+admin.site.register(Elemento)
+admin.site.register(Notificacion)
